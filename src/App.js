3ca093bd21882a14e4e32ba54da79a6e1edd8200
4850@ru! <captainphp@yandex.ru>
2023-04-24 01:01:57 +0300

    after bilda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaList } from 'react-icons/fa';
+import Catalog from './Catalog/Catalog';
+import CatalogId from './Catalog/CatalogId';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
+
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        {/* <FaList style={{ color: '#fc8507', fontSize: '30px', marginLeft: '10px'}} /> */}
+        
+        <Router>
+        {/* <Navigation /> */}
+
+        <Routes>
+          
+          <Route exact path="/" element={<Catalog/>} />
+          <Route path="/:tovarList" element={<CatalogId/>} />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
